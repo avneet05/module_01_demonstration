cb32648674a84c9a93deffc7bf24fe0401ea7883
avneet kaur <avneetkaur5@academic.rrc.ca>
2023-09-02 17:45:57 -0500

    Added a french hello world to python file

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+"""
+Description: Module 1 Demonstration
+Author: Avneet Kaur
+Date: August 29 2023
+Usage: Testing out GIT fot the first time
+"""
+print('Hello World')
+print("Bonjours tous le monde")
